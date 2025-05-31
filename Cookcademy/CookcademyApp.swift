2ccee51a83fa6969541e06d2910ed97ed63766ac
Zer0 <maelcolome.pro@icloud.com>
2025-05-31 19:49:46 +0200

    Initial commit with proper .gitignore

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Maël Colomé on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
