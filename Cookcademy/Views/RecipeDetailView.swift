4cd447fe2559d56f9828d7ab754fa0a95fe67b2c
Zer0 <maelcolome.pro@icloud.com>
2025-06-07 19:10:07 +0200

    ♻️ F02: Cookcademy > View > Add Design system to RecipeDetailView and Refacto to use RecipeData

--- Cookcademy/Views/RecipeDetailView.swift
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
+    let recipe: Recipe
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Author: \(recipe.mainInformation.author)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            
+            HStack {
+                Text("\(recipe.mainInformation.description)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            
+            List {
+                Section(header: Text("Ingredient")) {
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                        let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                            .foregroundColor(listTextColor)
+                    }.padding()
+                }.listRowBackground(listBackgroundColor)
+                
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let directions = recipe.directions[index]
+                        HStack {
+                            Text("\(index + 1).").bold().padding(.trailing)
+                            Text("\(directions.isOptional ? "(Optional) " : "")" + "\(directions.description)")
+                        }.foregroundColor(listTextColor)
+                    }.padding()
+                }.listRowBackground(listBackgroundColor)
+            }
+        }
+        .navigationTitle(recipe.mainInformation.name)
     }
 }
 
-#Preview {
-    RecipeDetailView()
+struct RecipeDetailView_Previews: PreviewProvider {
+  @State static var recipe = Recipe.testRecipes[0]
+  static var previews: some View {
+    NavigationView {
+      RecipeDetailView(recipe: recipe)
+    }
+  }
 }
