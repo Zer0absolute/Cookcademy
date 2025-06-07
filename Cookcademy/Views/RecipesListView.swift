b8f77133bbfa5dfebc25571d712c63f3bcf7f586
Zer0 <maelcolome.pro@icloud.com>
2025-06-07 19:06:36 +0200

    🚚 F02: Cookcademy > View > Move RecipeListView in View file

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -8,18 +8,39 @@
 import SwiftUI
 
 struct RecipesListView: View {
+    @State var recipeData = RecipeData()
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+            }
+            .padding()
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
-        .padding()
+        .navigationTitle(navigationTitle)
     }
 }
 
-#Preview {
-    RecipesListView()
+extension RecipesListView {
+  var recipes: [Recipe] {
+    recipeData.recipes
+  }
+  
+  var navigationTitle: String {
+    "All Recipes"
+  }
+}
+
+
+struct RecipesListView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationView {
+      RecipesListView()
+    }
+  }
 }
