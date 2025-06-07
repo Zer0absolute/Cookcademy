f2c559bbaaef31b1c172181966e8876a8a3d4dbc
Zer0 <maelcolome.pro@icloud.com>
2025-06-07 23:00:36 +0200

    ✨ F03: Cookcademy > ViewModels > RecipeData > add recipes(for:) function to filter recipes by category in RecipeData

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -10,4 +10,14 @@ import Foundation
 @Observable
 class RecipeData {
   var recipes = Recipe.testRecipes
+    
+    func recipes(for category: MainInformation.Category) -> [Recipe] {
+        var filteredRecipes = [Recipe]()
+        for recipe in recipes {
+          if recipe.mainInformation.category == category {
+            filteredRecipes.append(recipe)
+          }
+        }
+        return filteredRecipes
+    }
 }
