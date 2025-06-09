b936f16b908bc303da5b932137cb9e1acdc03673
Zer0 <maelcolome.pro@icloud.com>
2025-06-09 18:38:01 +0200

    ✅ F06: Cookcademy > Models > Recipe > Add init recipe

---
+++ Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyIngredientView.swift
+//  Cookcademy
+//
+//  Created by Maël Colomé on 08/06/2025.
+//
+
+import SwiftUI
+
+struct ModifyIngredientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyIngredientView()
+}
---
+++ Cookcademy/Views/ModifyRecipes/ModifyIngredientsView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyIngredientsView.swift
+//  Cookcademy
+//
+//  Created by Maël Colomé on 08/06/2025.
+//
+
+import SwiftUI
+
+struct ModifyIngredientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyIngredientsView()
+}
